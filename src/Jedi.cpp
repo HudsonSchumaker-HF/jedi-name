33bd1c6e94e39be347ae2e6826c8b0a3bf9aead7
Hudson Schumaker <155466722+HudsonSchumaker-HF@users.noreply.github.com>
2024-04-08 14:49:37 +0200

    Create Jedi.cpp

---
+++ src/Jedi.cpp
@@ -0,0 +1,79 @@
+/**
+* @file jedi.cpp
+* @author Hudson Schumaker
+* @brief This program generates a Jedi name based on user input.
+* The Jedi name is created by combining parts of the user's last name, first name, mother's name, and city name.
+*/
+#include <cctype>
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+#ifdef _WIN32
+#define CLEAR "cls"
+#else //In any other OS
+#define CLEAR "clear"
+#endif
+
+/**
+* @brief Converts all characters in a string to lowercase.
+* @param str The string to convert.
+*/
+void toLowerCase(std::string &str);
+
+/**
+* @brief Prints a prompt to the console and returns the user's input as a string.
+* @param prompt The prompt to display.
+* @return The user's input.
+*/
+std::string getInput(const std::string &prompt);
+
+/**
+* @brief Generates a Jedi name based on the provided names.
+* @param lastName The user's last name.
+* @param firstName The user's first name.
+* @param motherName The user's mother's name.
+* @param cityName The user's city name.
+* @return The generated Jedi name.
+*/
+std::string generateJediName(const std::string &lastName, const std::string &firstName, const std::string &motherName, const std::string &cityName);
+
+int main(void) {
+    std::system(CLEAR);
+    std::cout << "---- Jedi Name Creation ----" << std::endl;
+    std::string lastName, firstName, motherName, cityName;
+
+    lastName = getInput("Enter your last name: ");
+    firstName = getInput("Enter your first name: ");
+    motherName = getInput("Enter your mother's name: ");
+    cityName = getInput("Enter your city name: ");
+
+    toLowerCase(lastName);
+    toLowerCase(firstName);
+    toLowerCase(motherName);
+    toLowerCase(cityName);
+
+    std::string jediName = generateJediName(lastName, firstName, motherName, cityName);
+
+    std::cout << "Your Jedi Name is: " << jediName << std::endl;
+    return 0;
+}
+
+std::string getInput(const std::string &prompt) {
+    std::string input;
+    std::cout << prompt;
+    std::getline(std::cin, input);
+    std::system(CLEAR);  // Clear the terminal screen
+    std::cout << "---- Jedi Name Creation ----" << std::endl;
+    return input;
+}
+
+std::string generateJediName(const std::string &lastName, const std::string &firstName, const std::string &motherName, const std::string &cityName) {
+    std::string jediName = lastName.substr(0, 3) + firstName.substr(0, 2) + motherName.substr(0, 2) + cityName.substr(0, 3);
+    jediName[0] = std::toupper(jediName[0]);
+    return jediName;
+}
+
+void toLowerCase(std::string &str) {
+    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+}
